833ad1ea00cbd1b90ec7ed2ac914c0cfc2d3bde0
taiduo92 <taiduo@163.com>
2019-01-17 16:20:01 +0800

    提交域名

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,11 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<td_cms.github.io>.github.io
-# git push -f git@github.com:<td_cms.github.io>/<td_cms.github.io>.github.io.git master
 
-# if you are deploying to https://<td_cms.github.io>.github.io/<REPO>
-# git push -f git@github.com:<td_cms.github.io>/<REPO>.git master:gh-pages
+# if you are deploying to https://<td_cms>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
